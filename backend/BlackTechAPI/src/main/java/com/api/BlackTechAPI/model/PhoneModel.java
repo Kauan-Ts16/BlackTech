e58d768e0b8403162380e2305d084ad66aab2ae8
Kauan-Ts16 <Kauanfranca16@gmail.com>
2024-12-03 20:09:01 -0300

    Criação da PhoneModel com relacionamento OneToMany com UserModel.

---
+++ backend/BlackTechAPI/src/main/java/com/api/BlackTechAPI/model/PhoneModel.java
@@ -0,0 +1,35 @@
+package com.api.BlackTechAPI.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_phones")
+public class PhoneModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "phone_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer phoneId;
+
+    @Column(nullable = false, unique = true, length = 11)
+    private String number;
+
+    @Column(nullable = false, length = 5)
+    private String countryCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserModel userModel;
+}
