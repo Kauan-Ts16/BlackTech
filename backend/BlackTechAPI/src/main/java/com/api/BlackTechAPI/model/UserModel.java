9da3b2354fbba71af93848416a2f0076896be509
Kauan-Ts16 <Kauanfranca16@gmail.com>
2024-12-03 20:05:43 -0300

    Implementação da UserModel com relacionamentos OneToMany com AddressModel e PhoneModel, e ManyToOne com RoleModel.

---
+++ backend/BlackTechAPI/src/main/java/com/api/BlackTechAPI/model/UserModel.java
@@ -0,0 +1,47 @@
+package com.api.BlackTechAPI.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_users")
+public class UserModel implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "userModel")
+    private List<PhoneModel> phoneList = new ArrayList<>();
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "userModel")
+    private List<AddressModel> addressList = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "role_id")
+    private RoleModel role;
+}
