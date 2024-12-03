b1093cc49be810816b6aef2605bf23ad613c9d5b
Kauan-Ts16 <Kauanfranca16@gmail.com>
2024-12-03 16:38:18 -0300

    Upload do Projeto.

---
+++ backend/BlackTechAPI/src/main/java/com/api/BlackTechAPI/BlackTechApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.BlackTechAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackTechApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackTechApiApplication.class, args);
+	}
+
+}
