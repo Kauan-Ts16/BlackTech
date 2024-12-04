24773c861106a37392a342be9c2314411962196b
Kauan-Ts16 <Kauanfranca16@gmail.com>
2024-12-04 17:31:27 -0300

    Implementação do método customizado ao RoleRepository (findByRoleName).

--- backend/BlackTechAPI/src/main/java/com/api/BlackTechAPI/repository/RoleRepository.java
+++ backend/BlackTechAPI/src/main/java/com/api/BlackTechAPI/repository/RoleRepository.java
@@ -1,5 +1,6 @@
 package com.api.BlackTechAPI.repository;
 
+import com.api.BlackTechAPI.enums.RoleName;
 import com.api.BlackTechAPI.model.RoleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,6 @@ import java.util.UUID;
 
 @Repository
 public interface RoleRepository extends JpaRepository<RoleModel, UUID> {
+
+    RoleModel findByRoleName(RoleName roleName);
 }
