e7812da1d6bbb7e1e6f4d2d688f3192a65794b27
Kauan-Ts16 <Kauanfranca16@gmail.com>
2024-12-07 17:34:04 -0300

    Implementação do método customizado (findAllByUserModel) ao AddressRepository.

--- backend/BlackTechAPI/src/main/java/com/api/BlackTechAPI/repository/AddressRepository.java
+++ backend/BlackTechAPI/src/main/java/com/api/BlackTechAPI/repository/AddressRepository.java
@@ -1,9 +1,14 @@
 package com.api.BlackTechAPI.repository;
 
 import com.api.BlackTechAPI.model.AddressModel;
+import com.api.BlackTechAPI.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AddressRepository extends JpaRepository<AddressModel, Integer> {
+
+    List<AddressModel> findAllByUserModel(UserModel userModel);
 }
