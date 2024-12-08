979ac848f89268e73279a2d2446643c654efae35
Kauan-Ts16 <Kauanfranca16@gmail.com>
2024-12-08 00:24:02 -0300

    Implementação dos métodos customizados (existsByNumberAndPostalCodeAndUserModel) e (deleteAllByUserId) ao AddressRepository.

--- backend/BlackTechAPI/src/main/java/com/api/BlackTechAPI/repository/AddressRepository.java
+++ backend/BlackTechAPI/src/main/java/com/api/BlackTechAPI/repository/AddressRepository.java
@@ -3,12 +3,22 @@ package com.api.BlackTechAPI.repository;
 import com.api.BlackTechAPI.model.AddressModel;
 import com.api.BlackTechAPI.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface AddressRepository extends JpaRepository<AddressModel, Integer> {
 
     List<AddressModel> findAllByUserModel(UserModel userModel);
+
+    boolean existsByNumberAndPostalCodeAndUserModel(Integer number, String postalCode, UserModel userModel);
+
+    @Modifying
+    @Query("DELETE FROM AddressModel a WHERE a.userModel.userId = ?1")
+    void deleteAllByUserId(UUID userId);
 }
