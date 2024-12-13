5d00842fdf1123c14ab68095c6be37318b9449ee
Kauan-Ts16 <Kauanfranca16@gmail.com>
2024-12-12 21:40:29 -0300

    Implementação dos métodos customizados (findAllByUserModel), (existsByNumber) e (deleteAllByUserId) ao PhoneRepository.

--- backend/BlackTechAPI/src/main/java/com/api/BlackTechAPI/repository/PhoneRepository.java
+++ backend/BlackTechAPI/src/main/java/com/api/BlackTechAPI/repository/PhoneRepository.java
@@ -1,9 +1,23 @@
 package com.api.BlackTechAPI.repository;
 
 import com.api.BlackTechAPI.model.PhoneModel;
+import com.api.BlackTechAPI.model.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
 
 @Repository
 public interface PhoneRepository extends JpaRepository<PhoneModel, Integer> {
+
+    List<PhoneModel> findAllByUserModel(UserModel userModel);
+
+    boolean existsByNumber(String number);
+
+    @Modifying
+    @Query("DELETE FROM PhoneModel p WHERE p.userModel.userId = ?1")
+    void deleteAllByUserId(UUID userId);
 }
