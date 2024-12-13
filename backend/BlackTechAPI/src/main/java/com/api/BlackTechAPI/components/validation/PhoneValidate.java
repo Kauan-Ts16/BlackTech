09f17ba016546daa5a11769543418e418f1b2bc1
Kauan-Ts16 <Kauanfranca16@gmail.com>
2024-12-13 12:03:44 -0300

    Refatoração: Reorganização e implementação de um novo método para validar o limite de telefones.

--- backend/BlackTechAPI/src/main/java/com/api/BlackTechAPI/components/validation/PhoneValidate.java
+++ backend/BlackTechAPI/src/main/java/com/api/BlackTechAPI/components/validation/PhoneValidate.java
@@ -17,7 +17,8 @@ public class PhoneValidate {
     private PhoneRepository phoneRepository;
 
 
-    public void isValid(PhonePost phonePost) {
+    public void isValid(PhonePost phonePost, UserModel userModel) {
+        validateUserPhoneLimit(userModel);
         validatePhone(phonePost.number(), phonePost.areaCode(), phonePost.countryCode());
     }
 
@@ -38,6 +39,36 @@ public class PhoneValidate {
         validateUniqueNumber(number);
     }
 
+    private void validateNumber(String number) {
+        if (!number.matches("^\\d{5}-\\d{4}$")) {
+            throw new ExceptionGeneric("Phone number is not valid!", "Number must follow the format XXXXX-XXXX and contain exactly 9 digits.", 400);
+        }
+    }
+
+    private void validateAreaCode(String areaCode) {
+        if (!areaCode.matches("^\\d{2}$")) {
+            throw new ExceptionGeneric("Area Code is not valid!", "Area Code must contain exactly 2 digits.", 400);
+        }
+    }
+
+    private void validateCountryCode(String countryCode) {
+        if (!countryCode.matches("^\\+\\d{1,3}$")) {
+            throw new ExceptionGeneric("Country Code is not valid!", "Country Code must start with a '+' and contain between 1 and 3 digits.", 400);
+        }
+    }
+
+    private void validateUniqueNumber(String number) {
+        if (phoneRepository.existsByNumber(number)) {
+            throw new ExceptionGeneric("Phone number already exists!", "The phone number '" + number + "' is already registered in the system.", 400);
+        }
+    }
+
+    private void validateUserPhoneLimit(UserModel userModel) {
+        if (phoneRepository.countByUserModel(userModel) >= 2) {
+            throw new ExceptionGeneric("Phone limit exceeded!", "User can have a maximum 2 phone numbers.", 400);
+        }
+    }
+
     private void validateUser(Integer id, UserModel userModel) {
         PhoneModel phoneModel = findById(id);
         boolean isOwner = userModel.getUserId().equals(phoneModel.getUserModel().getUserId());
@@ -45,30 +76,6 @@ public class PhoneValidate {
 
         if (!isOwner && !isAdmin) {
             throw new ExceptionGeneric("Unauthorized action!", "You do not have permission to delete this phone number.", 403);
-        }
-    }
-
-    public void validateNumber(String number) {
-        if (!number.matches("^\\d{5}-\\d{4}$")) {
-            throw new ExceptionGeneric("Phone number is not valid!", "Number must follow the format XXXXX-XXXX and contain exactly 9 digits.", 400);
-        }
-    }
-
-    public void validateAreaCode(String areaCode) {
-        if (!areaCode.matches("^\\d{2}$")) {
-            throw new ExceptionGeneric("Area Code is not valid!", "Area Code must contain exactly 2 digits.", 400);
-        }
-    }
-
-    public void validateCountryCode(String countryCode) {
-        if (!countryCode.matches("^\\+\\d{1,3}$")) {
-            throw new ExceptionGeneric("Country Code is not valid!", "Country Code must start with a '+' and contain between 1 and 3 digits.", 400);
-        }
-    }
-
-    public void validateUniqueNumber(String number) {
-        if (phoneRepository.existsByNumber(number)) {
-            throw new ExceptionGeneric("Phone number already exists!", "The phone number '" + number + "' is already registered in the system.", 400);
         }
     }
 
