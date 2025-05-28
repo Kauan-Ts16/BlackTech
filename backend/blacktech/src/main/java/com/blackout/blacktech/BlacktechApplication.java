2d176d110ac7539a16da0d9e01cb9ed3ffd3acaf
Kauan-Ts16 <kauanfranca16@gmail.com>
2025-05-28 09:26:50 -0300

    chore(backend): adicionar estrutura inicial do back-end

---
+++ backend/blacktech/src/main/java/com/blackout/blacktech/BlacktechApplication.java
@@ -0,0 +1,13 @@
+package com.blackout.blacktech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlacktechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlacktechApplication.class, args);
+	}
+
+}
---
+++ backend/blacktech/src/test/java/com/blackout/blacktech/BlacktechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blackout.blacktech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlacktechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
