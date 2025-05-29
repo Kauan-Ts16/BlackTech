428daf534dff88d95b76b1390beda920674400e0
Kauan-Ts16 <kauanfranca16@gmail.com>
2025-05-29 01:07:41 -0300

    chore(security): desabilitar SecurityAutoConfiguration temporariamente no projeto

--- backend/blacktech/src/main/java/com/blackout/blacktech/BlacktechApplication.java
+++ backend/blacktech/src/main/java/com/blackout/blacktech/BlacktechApplication.java
@@ -2,8 +2,9 @@ package com.blackout.blacktech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BlacktechApplication {
 
 	public static void main(String[] args) {
