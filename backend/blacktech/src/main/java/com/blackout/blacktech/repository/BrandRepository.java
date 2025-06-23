8e41ce33b7c2d7b3583520188943b4fea646c48c
Kauan-Ts16 <kauanfranca16@gmail.com>
2025-06-23 16:22:25 -0300

    feat(brand): adicionar consultas para status ativo/inativo e ignorar case no nome

--- backend/blacktech/src/main/java/com/blackout/blacktech/repository/BrandRepository.java
+++ backend/blacktech/src/main/java/com/blackout/blacktech/repository/BrandRepository.java
@@ -4,13 +4,26 @@ import com.blackout.blacktech.model.BrandModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BrandRepository extends JpaRepository<BrandModel, UUID> {
 
-    boolean existsByName(String name);
+    boolean existsByNameIgnoreCase(String name);
 
-    boolean existsByNameAndIdNot(String name, UUID id);
+    boolean existsByNameIgnoreCaseAndIdNot(String name, UUID id);
+
+    boolean existsByIdAndIsActiveTrue(UUID id);
+
+    boolean existsByIdAndIsActiveFalse(UUID id);
+
+    Optional<BrandModel> findByIdAndIsActiveTrue(UUID id);
+
+    Optional<BrandModel> findByIdAndIsActiveFalse(UUID id);
+
+    List<BrandModel> findAllByIsActiveTrue();
+
+    List<BrandModel> findAllByIsActiveFalse();
 }
